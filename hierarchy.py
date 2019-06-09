ae3d4086e741bd5e77a4b600bfe44b347f4aa33e
Matthew D'Ambrosio <48102443+DambrosiCode@users.noreply.github.com>
2019-06-09 13:28:01 -0400

    Create hierarchy.py

---
+++ hierarchy.py
@@ -0,0 +1,28 @@
+import csv 
+
+iden = []
+clade = []
+
+te = []
+
+#TE name and cladereference
+with open('reference.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        iden.append(row[0])
+        clade.append(row[1:3])
+        
+#TE annotation file        
+with open('annotation.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        te.append(row)
+        
+f = open('gas.Acu1-4.te.hier.txt', 'w+')
+f.write('id\tfamily\torder\n')
+for i in range(0, len(te)-1):
+    for j in range(0, len(iden)-1):
+        if str(te[i]).find(str(iden[j])) > -1:
+            f.write(''.join(te[i]) + '\t' + '\t'.join(clade[j]) + '\n')
+
+f.close() 
