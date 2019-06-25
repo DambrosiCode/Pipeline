e19933ad467836a2095f095f2e1be7a46e66bb95
Matthew D'Ambrosio <48102443+DambrosiCode@users.noreply.github.com>
2019-06-25 17:55:29 -0400

    Create run_pbs.sh

---
+++ bash/run_pbs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#replace with bash script location 
+bashloc=/C:/usr/documents/bash 
+
+#replace with location of fastq files
+fastqloc=/C:/usr/documents/fastqs
+
+while [ ! -d "$fastqloc/Sample_$pop/" ]; do
+  ls "$fastqloc"
+  echo 'Which population? ' 
+  read pop
+done
+
+while [ ! -f "$bashloc/$script.pbs" ]; do
+  cd $bashloc
+  ls *.pbs
+  echo 'Which script? ' 
+  read script
+done
+
+module load shared 
+module load torque
+
+if [ "$script" = "map" ]
+then
+  echo 'Which Read? '
+  read i
+
+  qsub -v I=$i,POP=$pop '$v/map.pbs'
+  
+elif [ "$script" = "merge" ]
+then
+  qsub -v I=$i,POP=$pop '$bashloc/merge.pbs'
+  
+elif [ "$script" = "split" ]
+then
+  qsub -v POP=$pop '$bashloc/split.pbs'
+
+elif [ "$script" = "restore" ]
+then
+  qsub -v POP=$pop '$bashloc/restore.pbs'
+elif [ "$script" = "gzip" ]
+then
+  qsub -v POP=$pop '$bashloc/gzip.pbs'
+elif [ "$script" = "merge_fastqs" ]
+then
+  qsub -v POP=$pop '$bashloc/merge_fastqs.pbs'
+elif [ "$script" = "rewrite" ]
+then
+  qsub -v POP=$pop '$bashloc/rewrite.pbs'
+  elif [ "$script" = "ppileup" ]
+then
+  qsub -v POP=$pop '$bashloc/ppileup.pbs'
+fi
